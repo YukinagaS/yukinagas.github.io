da595e1ad8b530caa25bc4a5333650df244bc20a
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2025-11-09 19:15:59 +0900

    Change image optimization config

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,9 @@ const nextConfig: NextConfig = {
   distDir: 'out',
   basePath: '/yukinaga-portfolio',
   assetPrefix: '/yukinaga-portfolio',
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
